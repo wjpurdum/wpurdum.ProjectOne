3dc15abc0f7d13e4c7414ac400253500cc244459
Whitney <whitney.purdum@gmail.com>
2017-04-01 15:20:10 -0400

    update

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 var userScore = 0
 var userName = ""
+var timer
 
 var questionOne {
 	question: "What is the feminine plural form of 'translators' (مترجم)?";
@@ -27,38 +28,8 @@ var questionTwo {
 }
 
 
-var questionThree {
-	question: "What is the feminine plural form of 'translators' (مترجم)?";
-	answers {
-		a: شنسيتبشسيب
-		b: متشيسبيش
-		c: شنسيتبشسيب
-		d: يشبيسب
-	}
-	value: 20
-	correct: false
-}
 
-var questionFour {
-	question: "What is the feminine plural form of 'translators' (مترجم)?";
-	answers {
-		a: شنسيتبشسيب
-		b: متشيسبيش
-		c: شنسيتبشسيب
-		d: يشبيسب
-	}
-	value: 20
-	correct: false
-}
 
-var questionFive {
-	question: "What is the feminine plural form of 'translators' (مترجم)?";
-	answers {
-		a: شنسيتبشسيب
-		b: متشيسبيش
-		c: شنسيتبشسيب
-		d: يشبيسب
-	}
-	value: 20
-	correct: false
-}
+
+
+
