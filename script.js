9705ff96f9f3e11bf09c8aa2ae85d379161b20f2
Whitney <whitney.purdum@gmail.com>
2017-04-01 14:54:24 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var userScore = 0
+var userName = ""
+
+var questionOne {
+	question: "What is the feminine plural form of 'translators' (مترجم)?";
+	answers {
+		a: شنسيتبشسيب
+		b: متشيسبيش
+		c: شنسيتبشسيب
+		d: يشبيسب
+	}
+	value: 10
+	correct: false
+}dfsasdfdsس
