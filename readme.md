337dcf3bf142efb1d73adc164a3a11725f86cd3d
Whitney <whitney.purdum@gmail.com>
2017-04-03 16:46:57 -0400

    update

---
+++ readme.md
@@ -0,0 +1,15 @@
+Game Choice: Trivia/Quiz
+
+As a user, I should be able to: 
+1. Click through 5-10 trivia questions on basic Arabic grammar concepts. 
+2. Have each question tell me if I selected the correct or incorrect answer
+3. See my score in the top left corner of the screen as I progress through the quiz
+5. See how much time I have left to answer each question - counting down from 30 seconds for each questions
+6. See my final score and answers submitted
+
+Bronze: 5 unique questions will populate and quiz will flow based on user input. Final score will be tracked on each page in the upper lefthand corner. 
+
+Silver: Each question will have a 30 second time limit. After time limit hits zero, the question will disappear if it hasn't been answered. 
+
+Gold: Each question will have a 30 second time limit. ALl transitions between questions and screens will have a fade it/fade out function, instead of an abrupt switch. The timer will be fully functional. 
+
--- script.js
+++ script.js
@@ -1,31 +1,176 @@
 
-var userScore = 0
-var userName = ""
-var timer
+$(document).ready(function(){
 
-var questionOne {
-	question: "What is the feminine plural form of 'translators' (مترجم)?";
-	answers {
-		a: شنسيتبشسيب
-		b: متشيسبيش
-		c: شنسيتبشسيب
-		d: يشبيسب
-	}
-	value: 20
-	correct: false
+
+var questionOne = {
+	question: "What is the feminine plural form of 'translators' (مترجم)?",
+	answers: {
+		a: "شنسيتبشسيب",
+		b: "متشيسبيش",
+		c: "شنسيتبشسيب",
+		d: "يشبيسب",
+	},
+	value: 20,
+	correct: false,
+	sorry: "Sorry, that's incorrect.",
+	congratulations: "Great job, let's head to question 2",
 }
 
-var questionTwo {
-	question: "What is the feminine plural form of 'translators' (مترجم)?";
-	answers {
-		a: شنسيتبشسيب
-		b: متشيسبيش
-		c: شنسيتبشسيب
-		d: يشبيسب
-	}
-	value: 20
-	correct: false
+
+
+var questionTwo = {
+	question: "Question 2 (مترجم)?",
+	answers: {
+		a: "شنسيتبشسيب",
+		b: "متشيسبيش",
+		c: "شنسيتبشسيب",
+		d: "يشبيسب",
+	},
+	value: 20,
+	correct: false,
+	sorry: "Sorry, that's incorrect Question 2.",
+	congratulations: "Great job, let's head to question 3",
+
 }
+
+
+
+
+$("#start").click(startGame);
+
+
+
+function startGame() {
+	console.log('button has been clicked');
+	var userName = $("#enterName").val();
+	$("#name").text(userName);
+	$("#startScreen").addClass('hide');
+	showQuestionOne();
+};
+
+function clearText() {
+	console.log("clearText working");
+	$("#question").text("");
+	$("#answers").text("");
+	$("#question-text").text("");
+	$("#answer-one").text("");
+	$("#answer-two").text("");
+	$("#answer-three").text("");
+	$("#answer-four").text("");
+
+}
+
+function showQuestionOne() {
+	console.log('this function is registering');
+	$("#question,#answers,#next").removeClass('initialHide');
+	$("#question-text").text(questionOne.question);
+	$("#answer-one").text(questionOne.answers.a);
+	$("#answer-two").text(questionOne.answers.b);
+	$("#answer-three").text(questionOne.answers.c);
+	$("#answer-four").text(questionOne.answers.d);
+	$("#answerOneButton").click(function() {
+		console.log("correct!");
+		questionOne.correct = true;
+		var userScore = 20;
+		$("#score").text(userScore);
+		$("#answerOneButton").addClass('correct');
+		$("#hint").text(questionOne.congratulations)
+	});
+	$("#answerTwoButton").click(function() {
+		console.log("incorrect!");
+		questionOne.correct = false;
+		$("#answerTwoButton").addClass('incorrect');
+		$("#hint").text(questionOne.sorry)
+	});
+
+	$("#answerThreeButton").click(function() {
+		console.log("incorrect!");
+		questionOne.correct = false;
+		$("#answerThreeButton").addClass('incorrect');
+		$("#hint").text(questionOne.sorry)
+	});
+
+	$("#answerFourButton").click(function() {
+		console.log("incorrect!");
+		questionOne.correct = false;
+		$("#answerFourButton").addClass('incorrect');
+		$("#hint").text(questionOne.sorry)
+	});
+
+	$("#next").click(showQuestionTwo);
+
+};
+
+	function showQuestionTwo() {
+		console.log('question two loading');
+		$("#previous").removeClass("initialHide");
+		$("#question-text").text(questionTwo.question);
+		$("#answer-one").text(questionTwo.answers.a);
+		$("#answer-two").text(questionTwo.answers.b);
+		$("#answer-three").text(questionTwo.answers.c);
+		$("#answer-four").text(questionTwo.answers.d);
+		$("#answerOneButton").click(function() {
+			console.log("correct!");
+			questionTwo.correct = true;
+			var userScore = 20;
+			$("#score").text(userScore);
+			$("#answerOneButton").addClass('correct');
+			$("#hint").text(questionTwo.congratulations)
+			});
+		$("#answerTwoButton").click(function() {
+		console.log("incorrect!");
+		questionTwo.correct = false;
+		$("#answerTwoButton").addClass('incorrect');
+		$("#hint").text(questionTwo.sorry)
+	});
+
+		$("#answerThreeButton").click(function() {
+		console.log("incorrect!");
+		questionTwo.correct = false;
+		$("#answerThreeButton").addClass('incorrect');
+		$("#hint").text(questionTwo.sorry)
+	});
+
+		$("#answerFourButton").click(function() {
+		console.log("incorrect!");
+		questionTwo.correct = false;
+		$("#answerFourButton").addClass('incorrect');
+		$("#hint").text(questionTwo.sorry)
+	});
+
+	$("#next").click(showQuestionThree());
+
+	}
+
+
+
+
+
+
+
+
+
+//end closing
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
